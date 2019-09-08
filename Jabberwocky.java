52769b34ad4b9808911db20d49116075141cb1bb
n8srumsey <nathan.s.rumsey@gmail.com>
2019-09-07 17:57:39 -0700

    Updated to put into IntelliJ IDEA.

---
+++ Jabberwocky.java
@@ -0,0 +1,14 @@
+/**
+ * Jabberwocky assignment.
+ *
+ * @author Nate Rumsey
+ * @version 1.0
+ */
+public class Jabberwocky {
+    public static void main(String[] args) {
+        System.out.print("'twas brillig, and the slithy toves\n");
+        System.out.print("Did gyre and gimble in the wabe:\n");
+        System.out.print("All mimsy were the \"borogroves\"/\n");
+        System.out.print("And the mome raths out grabe.'");
+    }
+}
