89a85db19cbd800c3857eeb12443c90142732823
n8srumsey <nathan.s.rumsey@gmail.com>
2019-09-09 23:55:30 -0700

    Continued to update/change the Unit2Assingment class to fit needs of evaluating values.
    Signed-off-by: n8srumsey <nathan.s.rumsey@gmail.com>

--- Unit2Assignment1.java
+++ Unit2Assignment1.java
@@ -1,14 +1,18 @@
 public class Unit2Assignment1 {
     public static void main(String[] args) {
-        int num1 = 25;
-        int num2 = 40;
-        int num3 = 17;
-        int num4 = 5;
-        double val1 = 17.0;
-        double val2 = 12.78;
-        int iResult = num2&num4;
-        double fResult = (int) ((double) num1/num4);
-        System.out.println("iResult: " + iResult);
-        System.out.println("fResult: " + fResult);
+        double x = 2.5;
+        double y = -1.5;
+        int m = 18;
+        int n = 4;
+        String s = "Hello";
+        String t = "World";
+        System.out.println(x + n * y - (x + n) * y);
+        System.out.println(m / n + m % n);
+        System.out.println(5 * x - (double) n / 5);
+        System.out.println(s + t);
+        System.out.println(s + n);
+        System.out.println(1 - (1 - (1 - (1 - (1 - n)))));
+        System.out.println(s.substring(1, 3) + t.substring(4));
+        System.out.println(s.length() + t.length());
     }
 }
