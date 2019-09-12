1b0146c839b543522def561685ed9a1482ed6cf9
n8srumsey <nathan.s.rumsey@gmail.com>
2019-09-12 09:54:59 -0700

    Changed Geometry circumfrence to double.
    Signed-off-by: n8srumsey <nathan.s.rumsey@gmail.com>

--- ScanPractice.java
+++ ScanPractice.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.text.DecimalFormat;
+// import java.text.DecimalFormat;
 
 /**
  * Write a description of class ScanPractice here.
--- StringMutation.java
+++ StringMutation.java
@@ -1,6 +1,3 @@
-import java.util.*;
-
-
 /**
  * Play with Strings
  *
@@ -11,20 +8,28 @@ public class StringMutation
 {
     public static void main(String[] args)
     {
-        String phrase= new String("Change is inevitable");
-        
-       String mut1, mut2, mut3, mut4;
-       
-       Integer obj2 = new Integer(12);
-       int num2;
-       num2 = obj2;
-       
-       
-       Math.pow(7,4);
-       
-       
-       Scanner scan = new Scanner(System.in);
-       
+        String phrase = "Change is inevitable";
+        System.out.println(phrase);
+        int phraseLength = phrase.length();
+        System.out.println("Length of phrase = " + phraseLength);
+        String mut1, mut2, mut3, mut4;
+
+        mut1 = phrase + ", except from vending machines.";
+        System.out.println("mut1: " + mut1);
+        System.out.println("Length of mut1 = " + mut1.length());
+
+        mut2 = mut1.toUpperCase();
+        System.out.println("mut2: " + mut2);
+        System.out.println("Length of mut2 = " + mut2.length());
+
+        mut3 = mut2.replace('E', 'X');
+        System.out.println("mut3: " + mut3);
+        System.out.println("Length of mut3 = " + mut3.length());
+
+        mut4 = mut3.substring(mut3.indexOf('N'), mut3.indexOf('F') + 1);
+        System.out.println("mut4: " + mut4);
+        System.out.println("Length of mut4 = " + mut4.length());
+
     }
 } 
     
