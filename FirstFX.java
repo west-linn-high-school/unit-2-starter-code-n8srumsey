afdf0091e42a5ef9998c57e4ad578381592863a0
n8srumsey <nathan.s.rumsey@gmail.com>
2019-09-24 09:54:33 -0700

    Continued to work on FirstFX.
    Signed-off-by: n8srumsey <nathan.s.rumsey@gmail.com>

--- FirstFX.java
+++ FirstFX.java
@@ -2,14 +2,12 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
-
 
 public class FirstFX extends Application {
 
@@ -18,33 +16,71 @@ public class FirstFX extends Application {
     }
 
     public void start(Stage stage) {
-        Text console = new Text(50, 50, ">>");
-        console.setFont(new Font(100));
-        Rectangle box = new Rectangle(20, 20, 120, 50);
-        Circle circle = new Circle();
-        circle.setCenterX(400.0);
-        circle.setCenterY(500.0);
-        circle.setRadius(100.0);
-        Arc arc = new Arc();
-        arc.setCenterX(1000.0);
-        arc.setCenterY(500.0);
-        arc.setRadiusX(600.0);
-        arc.setRadiusY(300.0);
-        arc.setStartAngle(30.0);
-        arc.setLength(1000.0);
-        arc.setType(ArcType.ROUND);
+        Text console = new Text(17.5, 25, ">> _");
+        console.setFont(new Font(25));
+        console.setStroke(Color.WHITE);
+        console.setFill(Color.WHITE);
 
+        Text exit = new Text(540, 187, "X");
+        exit.setFont(new Font(12));
+        exit.setStroke(Color.RED);
+        exit.setFill(Color.RED);
 
+        Text minimize = new Text(510, 187, "-");
+        minimize.setFont(new Font(12));
+        minimize.setStroke(Color.BLACK);
+        minimize.setFill(Color.BLACK);
+
+        Text errorTitle = new Text(260, 187, "Error - 404");
+        errorTitle.setFont(new Font(12));
+        errorTitle.setStroke(Color.BLACK);
+        errorTitle.setFill(Color.BLACK);
+
+        Text errorMessage = new Text(390, 225, "404");
+        errorMessage.setFont(new Font(15));
+        errorMessage.setStroke(Color.BLACK);
+        errorMessage.setFill(Color.BLACK);
+
+        Text errorSubMessage = new Text(350, 245, "Website Not Found");
+
+        Line line = new Line();
+        line.setStartX(250.0);
+        line.setStartY(193.0);
+        line.setEndX(550.0);
+        line.setEndY(193.0);
+
+        Rectangle box = new Rectangle();
+        box.setX(1.0);
+        box.setY(1.0);
+        box.setWidth(998.0);
+        box.setHeight(498.0);
         box.setStroke(Color.WHITESMOKE);
         box.setFill(null);
 
-        circle.setStroke(Color.RED);
-        circle.setFill(null);
+        Rectangle fullscreen = new Rectangle();
+        fullscreen.setX(525.0);
+        fullscreen.setY(179);
+        fullscreen.setWidth(7.0);
+        fullscreen.setHeight(7.0);
+        fullscreen.setFill(null);
+        fullscreen.setStroke(Color.BLACK);
 
-        arc.setStroke(Color.CADETBLUE);
-        arc.setFill(null);
 
-        Group root = new Group(console, box, circle, arc);
+        Rectangle errorWindow = new Rectangle();
+        errorWindow.setX(250.0);
+        errorWindow.setY(175.0);
+        errorWindow.setWidth(300.0);
+        errorWindow.setHeight(120.0);
+        errorWindow.setStroke(Color.WHITESMOKE);
+        errorWindow.setFill(Color.LIGHTGREY);
+
+        Translate translation = new Translate(300, 50.0);
+
+
+        Group errorPopUp = new Group(errorWindow, exit, minimize, line, fullscreen, errorTitle, errorMessage,
+                errorSubMessage);
+        errorPopUp.getTransforms().addAll(translation);
+        Group root = new Group(console, box, errorPopUp);
         Scene scene = new Scene(root, 1000, 500, Color.BLACK);
 
         stage.setTitle("FXPractice");
